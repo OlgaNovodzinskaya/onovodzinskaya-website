7b69b457ea46e08ad8f42c26b961bbb60e004c93
jakhongir-abdukhamidov <jakhongirabdukhamidov@gmail.com>
2023-02-28 10:52:31 +0500

    conect to supabase"

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,23 +1,83 @@
+import { getAllFlats } from "@web/services/supabase";
+import classNames from "classnames/bind";
 import "keen-slider/keen-slider.min.css";
 import { useKeenSlider } from "keen-slider/react";
-// import "./Carousel.scss";
+import { useState } from "react";
+import useSWR from "swr";
+import ObjectCard from "../Objects/ObjectCard";
+import styles from "./Carousel.module.scss";
+
+const cn = classNames.bind(styles);
 
 export default function Carousel() {
-  const [ref] = useKeenSlider<HTMLDivElement>({
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+
+  const [ref, instanceRef] = useKeenSlider<HTMLDivElement>({
     slides: {
-      perView: 2,
+      perView: 1,
       spacing: 15,
+    },
+    breakpoints: {
+      "(min-width: 768px)": {
+        slides: {
+          perView: 2,
+          spacing: 15,
+        },
+      },
+      "(min-width: 1024px)": {
+        slides: {
+          perView: 3,
+          spacing: 15,
+        },
+      },
+
+      "(min-width: 1280px)": {
+        slides: {
+          perView: 4,
+          spacing: 15,
+        },
+      },
+    },
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
     },
   });
 
+  const { data, error, isLoading } = useSWR("/api/flats", getAllFlats);
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
-    <div ref={ref} className="keen-slider">
-      <div className="keen-slider__slide number-slide1">1</div>
-      <div className="keen-slider__slide number-slide2">2</div>
-      <div className="keen-slider__slide number-slide3">3</div>
-      <div className="keen-slider__slide number-slide4">4</div>
-      <div className="keen-slider__slide number-slide5">5</div>
-      <div className="keen-slider__slide number-slide6">6</div>
+    <div className="container">
+      <div ref={ref} className={cn("keen-slider slider_container")}>
+        {data?.map((item) => {
+          return <ObjectCard key={item.id} {...item} />;
+        })}
+      </div>
+
+      <div className="mt-4">
+        {loaded && instanceRef.current && (
+          <div className={cn("dots")}>
+            {new Array(2).fill(5).map((_, idx) => {
+              return (
+                <button
+                  key={idx}
+                  onClick={() => {
+                    instanceRef.current?.moveToIdx(idx);
+                  }}
+                  className={cn("dot", {
+                    "dot--active": currentSlide === idx,
+                  })}
+                ></button>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,15 @@
 export { default } from "@web/modules/home";
+import { getAllFlats } from "@web/services/supabase";
+import { GetStaticProps } from "next";
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await getAllFlats();
+
+  return {
+    props: {
+      fallback: {
+        "/api/flats": res,
+      },
+    },
+  };
+};
